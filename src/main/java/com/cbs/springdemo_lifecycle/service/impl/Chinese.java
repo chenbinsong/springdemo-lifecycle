5f51be8991f161ca2e8c22754ca6be544a55a4a1
chenbinsong <631373606@qq.com>
2016-09-18 16:54:54 +0800

    add destroy method

--- src/main/java/com/cbs/springdemo_lifecycle/service/impl/Chinese.java
+++ src/main/java/com/cbs/springdemo_lifecycle/service/impl/Chinese.java
@@ -1,17 +1,28 @@
 package com.cbs.springdemo_lifecycle.service.impl;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import com.cbs.springdemo_lifecycle.service.Person;
 
-public class Chinese implements Person,InitializingBean{
+public class Chinese implements Person,InitializingBean,DisposableBean{
 
+	/**
+	 * 初始化方法
+	 */
 	public void afterPropertiesSet() throws Exception {
 		System.out.println("执行初始化方法");
 	}
 
 	public String sayHi() {
 		return "你好";
+	}
+
+	/**
+	 * 销毁方法
+	 */
+	public void destroy() throws Exception {
+		System.out.println("执行销毁方法");
 	}
 	
 	
--- src/test/java/com/cbs/springdemo_lifecycle/test/SpringTest.java
+++ src/test/java/com/cbs/springdemo_lifecycle/test/SpringTest.java
@@ -2,6 +2,7 @@ package com.cbs.springdemo_lifecycle.test;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cbs.springdemo_lifecycle.service.Person;
@@ -10,9 +11,10 @@ public class SpringTest {
 	
 	@Test
 	public void test(){
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Person person = context.getBean("chinese",Person.class);
 		System.out.println(person.sayHi());
+		context.registerShutdownHook();
 	}
 
 }
